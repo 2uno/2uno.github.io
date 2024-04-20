c465d73f46694aadf040c987f091c41014cdf2ce
eunho0213 <eunho0213@gmail.com>
2024-04-20 18:52:03 +0900

    era morgetta

--- js/intro.js
+++
@@ -1,7 +0,0 @@
-function showWeddingCard() {
-  document.querySelector('.intro-container').style.display = 'none';
-  document.querySelector('.wedding-card').style.display = 'block';
-}
-
-// 페이지 로드 후 7초 후에 인트로 종료
-setTimeout(showWeddingCard, 7000);
